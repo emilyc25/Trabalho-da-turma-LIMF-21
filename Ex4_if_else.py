c2b6aebeadbd912a1a3b4ba7a41cf626ace64ea0
railsoncarmo30-ui <railsoncarmo30@gmail.com>
2025-11-19 10:38:16 -0300

    Add files via upload

---
+++ Ex4_if_else.py
@@ -0,0 +1,9 @@
+#4.if_else
+# Exemplo 4: Script que informa se um número é positivo ou negativo
+
+valor = float(input("Digite um valor numérico: "))
+if valor < 0:
+  print("Você digitou um valor negativo.")
+
+else:
+  print("Você digitou um valor positivo.")
